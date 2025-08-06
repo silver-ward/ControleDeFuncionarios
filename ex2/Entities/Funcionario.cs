d15637740d57d03a6f5c0054507628f8e04bcd72
Eduardo Ribeiro <eduard_ribeiro@hotmail.com>
2025-08-06 18:37:45 -0300

    Added DataAnnotations to Funcionario entity.

--- ex2/Entities/Funcionario.cs
+++ ex2/Entities/Funcionario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,21 @@ namespace ex2.Entities
     /// </summary>
     public class Funcionario
     {
-
+        [Required(ErrorMessage = "Algo deu errado ao atribuir o número de identificação do funcionário.\nPor favor, tente novamente ou contate o suporte.")]
         public required Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [MinLength(3, ErrorMessage = "Informe um nome com pelo menos {1} caracteres.")]
+        [MaxLength(100, ErrorMessage = "Informe um nome com no máximo {1} caracteres.")]
         public required string Nome { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [MinLength(5, ErrorMessage = "Informe uma matrícula com pelo menos {1} caracteres.")]
+        [MaxLength(10, ErrorMessage = "Informe uma matrícula com no máximo {1} caracteres.")]
         public required string Matricula { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$")]
         public required string Cpf { get; set; }
     }
 }
